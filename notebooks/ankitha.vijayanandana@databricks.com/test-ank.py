ec054108b65c9a4d706dcb8143c85742e4451d8b
Ankitha1310 <38287159+Ankitha1310@users.noreply.github.com>
2022-01-28 11:34:14 +0530

    first-commit from db

---
+++ notebooks/ankitha.vijayanandana@databricks.com/test-ank.py
@@ -0,0 +1,74 @@
+# Databricks notebook source
+1+1
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC sudo pip install awscli 
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
